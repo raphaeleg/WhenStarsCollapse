ae749dbfc9d6eaee6b2f1ad48bb3ee3087650101
raphaeleg <raphaeleguillemotmichelle@gmail.com>
2024-08-10 11:00:26 +0800

    Added base Game Event Scripts
    From previous projects

---
+++ DJam/Assets/Scripts/Game Events/Editor/GameEventEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GameEventSO), editorForChildClasses: true)]
+public class GameEventEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUI.enabled = Application.isPlaying;
+
+        GameEventSO e = target as GameEventSO;
+        if (GUILayout.Button("Raise")) { 
+            e.Raise(); 
+        }
+    }
+}
---
+++ DJam/Assets/Scripts/Game Events/GameEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameEventSO : ScriptableObject
+{
+    private readonly List<GameEventListener> eventListeners = new();
+
+    public void Raise()
+    {
+        for (int i = eventListeners.Count - 1; i >= 0; i--) { 
+            eventListeners[i].OnEventRaised(); 
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        if (eventListeners.Contains(listener)) { return; }
+        eventListeners.Add(listener); 
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        if (!eventListeners.Contains(listener)) { return; }
+        eventListeners.Remove(listener);
+    }
+}
