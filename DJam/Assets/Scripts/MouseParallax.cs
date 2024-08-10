13aa92940a25a5efb1d3ea9b74ec711b4e69c8f8
Nick MC <107901861+NickMC05@users.noreply.github.com>
2024-08-10 22:45:57 +0800

    Backgound Parallax

---
+++ DJam/Assets/Scripts/MouseParallax.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseParallax : MonoBehaviour
+{
+    public float cameraSpeed;
+    public float distanceRange = 1f;
+
+    // Camera follow mouse position
+    void FixedUpdate()
+    {
+        // Find mouse screen position
+        Vector2 pos = Input.mousePosition;
+        Vector2 playerPos = new Vector2(Screen.width / 2, Screen.height / 2);
+        float xPos = (pos.x - playerPos.x) / Screen.width;
+        float yPos = (pos.y - playerPos.y) / Screen.height;
+        Debug.Log(xPos + " " + yPos);
+
+        // Camera go to mouse
+        Vector2 targetPos = new Vector2(-xPos, -yPos);
+        transform.localPosition = Vector2.Lerp(transform.localPosition, targetPos*distanceRange, cameraSpeed * Time.deltaTime);
+
+    }
+}
