42d048619ba7ad58a096211700d89450659b5ca7
Nick MC <107901861+NickMC05@users.noreply.github.com>
2024-08-10 20:26:33 +0800

    Animation No Loop

---
+++ DJam/Assets/Scripts/AnimationNoLoop.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationNoLoop : MonoBehaviour
+{
+    public Sprite[] frames;
+
+    void Start()
+    {
+        StartCoroutine(Running());
+    }
+
+    IEnumerator Running()
+    {
+        for (int i = 0; i < frames.Length; i++)
+        {
+            gameObject.GetComponent<UnityEngine.UI.Image>().sprite = frames[i];
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
