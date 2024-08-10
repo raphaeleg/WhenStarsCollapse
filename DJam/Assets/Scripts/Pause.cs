9afb744d1def40038474c824f8cbe161449eccb4
Nick MC <107901861+NickMC05@users.noreply.github.com>
2024-08-10 22:21:19 +0800

    Pause Menu

---
+++ DJam/Assets/Scripts/Pause.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseUI;
+    private bool paused;
+
+    public void ButtonPressed()
+    {
+        paused = !paused;
+
+        if (paused)
+        {
+            Time.timeScale = 0;
+            pauseUI.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseUI.SetActive(false);
+        }
+    }
+}
