1f9b62510349fe91cbda1d3e5bd864a3f6bd0c06
Nick MC <107901861+NickMC05@users.noreply.github.com>
2024-08-10 20:55:57 +0800

    Tutorial Text

---
+++ DJam/Assets/Scripts/SetActiveButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveButton : MonoBehaviour
+{
+    public GameObject[] toActive;
+    public GameObject[] toNotActive;
+    public AudioSource sfx;
+    public bool getCard;
+
+    // SetActive changes when button is clicked
+    public void ButtonPressed()
+    {
+        if (sfx != null)
+        {
+            sfx.Play();
+        }
+
+        for (int i = 0; i < toActive.Length; i++)
+        {
+            toActive[i].SetActive(true);
+        }
+        for (int i = 0; i < toNotActive.Length; i++)
+        {
+            toNotActive[i].SetActive(false);
+        }
+    }
+}
