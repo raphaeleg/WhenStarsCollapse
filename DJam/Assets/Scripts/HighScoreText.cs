12b1af60967fd9569ec8707d97d3f0123b00f803
Nick MC <107901861+NickMC05@users.noreply.github.com>
2024-08-10 20:56:53 +0800

    HighScore

--- DJam/Assets/Scripts/HighScoreText.cs
+++ DJam/Assets/Scripts/HighScoreText.cs
@@ -14,7 +14,15 @@ public class HighScoreText : MonoBehaviour
 
     void Update()
     {
-        timer.text = highScore.timer.ToString();
+        int minute = highScore.timer / 60;
+        int second = highScore.timer % 60;
+        string minuteStr = minute.ToString();
+        if (minuteStr.Length == 1)
+            minuteStr = "0" + minuteStr;
+        string secondStr = second.ToString();
+        if (secondStr.Length == 1)
+            secondStr = "0" + secondStr;
+        timer.text = minuteStr + ":" + secondStr;
         stars.text = highScore.stars.ToString();
         holes.text = highScore.blackHoles.ToString();
     }
