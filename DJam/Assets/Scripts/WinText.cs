eb97cbaa82d7c79c0abe47c503f6452a98257de8
raphaeleg <raphaeleguillemotmichelle@gmail.com>
2024-08-12 00:32:39 +0800

    HighScore Animation Timing

--- DJam/Assets/Scripts/WinText.cs
+++ DJam/Assets/Scripts/WinText.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class WinText : MonoBehaviour
@@ -26,6 +27,20 @@ public class WinText : MonoBehaviour
     private const int multiplierStars = 200;
     private const int multiplierWhiteDwarfs = 250;
 
+    public GameObject title_timer;
+    public GameObject preText_timer;
+    public GameObject raw_timer;
+    public GameObject title_stars;
+    public GameObject preText_stars; 
+    public GameObject raw_stars;
+    public GameObject title_dwarf;
+    public GameObject preText_dwarf;
+    public GameObject raw_dwarf;
+    public GameObject title_final;
+    public GameObject final_btn;
+
+    private WaitForSeconds delay = new WaitForSeconds(1);
+
     void Start()
     {
         int minute = highScore.timer / 60;
@@ -46,12 +61,56 @@ public class WinText : MonoBehaviour
         resultStarsInt = (highScore.stars * multiplierStars);
         resultWhiteDwarfsInt = (highScore.whiteDwarfs * multiplierWhiteDwarfs);
 
-        resultTimer.text ="" + resultTimerInt;
-        resultStars.text ="" + resultStarsInt;
-        resultWhiteDwarfs.text ="" + resultWhiteDwarfsInt;
+        resultTimer.text = "0000";
+        resultStars.text ="0000";
+        resultWhiteDwarfs.text ="0000";
+        totalResult.text = "0000";
 
-        totalResult.text = "" + (resultTimerInt + resultStarsInt + resultWhiteDwarfsInt);
+        StartCoroutine(TimerAnim());
     }
 
+    IEnumerator TimerAnim()
+    {
+        yield return delay;
+        title_timer.SetActive(true);
+        preText_timer.SetActive(true);
+        yield return delay;
+        raw_timer.SetActive(true);
+        yield return delay;
+        resultTimer.text = "" + resultTimerInt;
+        StartCoroutine(StarsAnim());
+    }
 
+    IEnumerator StarsAnim()
+    {
+        yield return delay;
+        title_stars.SetActive(true);
+        preText_stars.SetActive(true);
+        yield return delay;
+        raw_stars.SetActive(true);
+        yield return delay;
+        resultStars.text = "" + resultStarsInt;
+        StartCoroutine(DwarfPart());
+    }
+    IEnumerator DwarfPart()
+    {
+        yield return delay;
+        title_dwarf.SetActive(true);
+        preText_dwarf.SetActive(true);
+        yield return delay;
+        raw_dwarf.SetActive(true);
+        yield return delay;
+        resultWhiteDwarfs.text = "" + resultWhiteDwarfsInt;
+        StartCoroutine(Total());
+    }
+
+    IEnumerator Total()
+    {
+        yield return delay;
+        title_final.SetActive(true);
+        yield return delay;
+        totalResult.text = "" + (resultTimerInt + resultStarsInt + resultWhiteDwarfsInt);
+        yield return delay;
+        final_btn.SetActive(true);
+    }
 }
