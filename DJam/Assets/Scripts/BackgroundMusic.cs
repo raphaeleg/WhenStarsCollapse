6b8724b473b61d6205561991c4b9d2f6c1f879ff
Dungeon Master <70334983+ItzCog@users.noreply.github.com>
2024-08-11 16:22:56 +0800

    added basic wwise integration
    supports play music on start and music volume adjustment

--- DJam/Assets/Scripts/BackgroundMusic.cs
+++ DJam/Assets/Scripts/BackgroundMusic.cs
@@ -1,20 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Rendering;
+using AK.Wwise;
 
 public class BackgroundMusic : MonoBehaviour
 {
-    // BGM
-    private AudioSource musicSource;
-    public AudioClip mainMenuBGM;
-    public AudioClip gameplayBGM;
-    public AudioClip gameOverBGM;
-
-    public AudioSource[] SFX; // Points to SFXs in scene
-
-    private static BackgroundMusic instance = null;
+    public static BackgroundMusic instance = null;
 
     public float soundEffects = 0.5f;
     public float musicVolume = 0.5f;
@@ -36,9 +27,11 @@ public class BackgroundMusic : MonoBehaviour
         }
     }
 
-    public void PlayMainMenuBGM() { musicSource.Stop(); musicSource.clip = mainMenuBGM; musicSource.Play(); }
-    public void PlayGameplayBGM() { musicSource.Stop(); musicSource.clip = gameplayBGM; musicSource.Play(); }
-    public void PlayGameOverBGM() { musicSource.Stop(); musicSource.clip = gameOverBGM; musicSource.Play(); }
+    private void Start()
+    {
+        AkSoundEngine.PostEvent("Game_Start", gameObject);
+        // AkSoundEngine.SetRTPCValue("Intensity", 100);
+    }
 
     public void SoundEffectsChanged()
     {
@@ -47,15 +40,7 @@ public class BackgroundMusic : MonoBehaviour
 
     public void MusicVolumeChanged()
     {
-        musicVolume = volumeSlider.value;
+        AkSoundEngine.SetRTPCValue("Volume", 100 * volumeSlider.value);
     }
 
-    private void Update()
-    {
-        for (int i = 0; i < SFX.Length; i++)
-        {
-            SFX[i].volume = soundEffects;
-        }
-        //musicSource.volume = musicVolume;
-    }
 }
