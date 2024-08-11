563a6ae22a7a2f832fc86d762d027a6942b79062
Nick MC <107901861+NickMC05@users.noreply.github.com>
2024-08-12 06:52:29 +0800

    Tutorial

--- DJam/Assets/Scripts/Animation.cs
+++ DJam/Assets/Scripts/Animation.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Animation : MonoBehaviour
 {
     public Sprite[] frames;
     public float rate = 0.1f;
+    public bool loop = true;
 
-    void Start()
+    public void Start()
     {
         StartCoroutine(Running());
     }
@@ -17,9 +19,11 @@ public class Animation : MonoBehaviour
         {
             for (int i = 0; i < frames.Length; i++)
             {
-                gameObject.GetComponent<UnityEngine.UI.Image>().sprite = frames[i];
+                gameObject.GetComponent<Image>().sprite = frames[i];
                 yield return new WaitForSeconds(rate);
             }
+            if (!loop)
+                break;
         }
     }
 }
