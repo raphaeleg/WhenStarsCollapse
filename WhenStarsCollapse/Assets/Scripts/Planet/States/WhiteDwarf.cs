33e2bd2139957f3b2a1dc392582a094ad2ba9611
raphaeleg <raphaeleguillemotmichelle@gmail.com>
2024-09-09 15:21:25 +0800

    White Dwarf Behaviour

--- WhenStarsCollapse/Assets/Scripts/Planet/States/WhiteDwarf.cs
+++ WhenStarsCollapse/Assets/Scripts/Planet/States/WhiteDwarf.cs
@@ -10,16 +10,18 @@ namespace Planets
         public WhiteDwarf(Planet planet) : base(planet) { }
 
         public int stage = 1;
+        private const int INTERVAL = 5;
+        private const int ANIM_DESTROY_TIME = 1;
 
         public override IEnumerator Start()
         {
             Planet.visuals.Anim_BecomeDwarf();
             EventManager.TriggerEvent("whiteDwarfSpawn", 0);
 
-            yield return new WaitForSeconds(20);
-            
+            yield return new WaitForSeconds(INTERVAL);
+
             Planet.visuals.Anim_DestroyDwarf();
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(ANIM_DESTROY_TIME);
             Planet.OnDestroy();
         }
     }
