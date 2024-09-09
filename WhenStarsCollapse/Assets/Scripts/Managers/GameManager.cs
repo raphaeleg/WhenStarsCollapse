e773ee9eecabbb2b7ee5ed083d90484d844d6ade
raphaeleg <raphaeleguillemotmichelle@gmail.com>
2024-09-09 15:45:32 +0800

    Gameplay Manager & ScoreManager
    scoremanager can now be treated like a bag of bits to rummage around

--- WhenStarsCollapse/Assets/Scripts/Managers/GameManager.cs
+++ WhenStarsCollapse/Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,53 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Check whether player win/lose
+    [SerializeField] ScoreManager score;
+    [SerializeField] const int BLACKHOLE_END_CONDITION = 1;
+    private Dictionary<string, Action<int>> SubscribedEvents;
+
+    private void Awake()
+    {
+        SubscribedEvents = new() {
+            { "isSuccessfulSpawn", Event_AddCount_Star },
+            { "blackHoleSpawn", Event_AddCount_BlackHole },
+            { "whiteDwarfSpawn", Event_AddCount_WhiteDwarf },
+        };
+    }
+    private void OnEnable()
+    {
+        foreach (var pair in SubscribedEvents)
+        {
+            EventManager.StartListening(pair.Key, pair.Value);
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var pair in SubscribedEvents)
+        {
+            EventManager.StopListening(pair.Key, pair.Value);
+        }
+    }
+
+    public void Start() { score.Restart(); }
+
+    public void Event_AddCount_Star(int val)
+    {
+        score.stars++;
+        EventManager.TriggerEvent("StarsText", score.stars);
+    }
+    public void Event_AddCount_WhiteDwarf(int val) { score.whiteDwarfs++; }
+    public void Event_AddCount_BlackHole(int val)
+    {
+        score.blackHoles++;
+        EventManager.TriggerEvent("BlackHoleText", score.blackHoles);
+        if (score.blackHoles is BLACKHOLE_END_CONDITION) { Lose(); }
+    }
+    private void Lose()
+    {
+        SceneLoader.LoadLoseScreen();
+    }
 }
--- WhenStarsCollapse/Assets/Scripts/Managers/ScoreManager.cs
+++ WhenStarsCollapse/Assets/Scripts/Managers/ScoreManager.cs
@@ -2,54 +2,17 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreManager : MonoBehaviour
+[CreateAssetMenu]
+public class ScoreManager : ScriptableObject
 {
-    private int stars = 0;
-    private int whiteDwarfs = 0;
-    private int blackHoles = 0;
-    private Dictionary<string, Action<int>> SubscribedEvents;
-
-    private void Awake()
-    {
-        SubscribedEvents = new() {
-            { "isSuccessfulSpawn", Event_AddCount_Star },
-            { "blackHoleSpawn", Event_AddCount_BlackHole },
-            { "whiteDwarfSpawn", Event_AddCount_WhiteDwarf },
-        };
-    }
-    private void OnEnable()
-    {
-        foreach (var pair in SubscribedEvents)
-        {
-            EventManager.StartListening(pair.Key, pair.Value);
-        }
-    }
-
-    private void OnDisable()
-    {
-        foreach (var pair in SubscribedEvents)
-        {
-            EventManager.StopListening(pair.Key, pair.Value);
-        }
-    }
-
-    
-    public void Start() { Restart(); }
-
-    public void Event_AddCount_Star(int val) { 
-        stars++;
-        EventManager.TriggerEvent("StarsText", stars);
-    }
-    public void Event_AddCount_WhiteDwarf(int val) { whiteDwarfs++; }
-    public void Event_AddCount_BlackHole(int val) { 
-        blackHoles++;
-        EventManager.TriggerEvent("BlackHoleText", blackHoles);
-    }
-    private void Restart()
+    public int stars = 0;
+    public int blackHoles = 0;
+    public int whiteDwarfs = 0;
+    public void Restart()
     {
         stars = 0;
         whiteDwarfs = 0;
         blackHoles = 0;
-        EventManager.TriggerEvent("RestartStats", 0);
+        //EventManager.TriggerEvent("RestartStats", 0);
     }
 }
